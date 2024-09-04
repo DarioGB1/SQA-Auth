da0724c2e1791d05b30e7d4e1fbdfda0d8e2513b
DarioGB1 <darioguzmanbz@gmail.com>
2024-09-04 02:26:11 -0400

    feature: Desarrollo de la API [Registro de usuario]

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use App\Models\User;
+use \stdClass;
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+        if ($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer',]);
+    }
+}
